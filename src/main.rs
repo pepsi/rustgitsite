a59f027f0de0aef21d7df39a9f26f9a867bb1cb9
Rusty Shackleford <a@neb.best>
2020-10-03 04:00:18 -0400

    2

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,13 @@ fn main() {
         Err(e) => panic!("failed to open: {}", e),
     };
     let b = repo.head().unwrap();
-    println!("{}", b.name().unwrap());
+    let mut revwalk = repo.revwalk().unwrap();
+    let base = git2::Sort::REVERSE;
+    revwalk.set_sorting(base).unwrap();
+    println!("st sorting to base");
+    for walk in revwalk{
+        println!("Walked {}", walk.unwrap());
+    }
+    println!("Latest commit {}", b.peel_to_commit().unwrap().summary().unwrap());
     // println!("{}", repo.statuses(None).unwrap());
-    println!("Hello, world!");
 }
