2b084ebe3484c8d785d4ae7b60ba68e657f5ae34
Rusty Shackleford <a@neb.best>
2020-10-03 14:34:08 -0400

    Stash new to master

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use git2::Repository;
-fn get_prev_commit(c: git2::Commit) -> Vec<git2::Commit>{
-    let mut p: Vec<git2::Commit> = vec![];
-    println!("c.parent_count {}", c.parent_count());
-    if c.parent_count() == 1{
-        return p;
-    }else{
-        for prev_commit in get_prev_commit(c){
-            p.push(prev_commit);
+
+fn get_all_commits(start: git2::Commit) -> Vec<git2::Commit> {
+    let mut p: Vec<git2::Commit> = vec![start.clone()];
+    for parent in start.parents() {
+        let x = get_all_commits(parent);
+        for y in x {
+            p.push(y);
         }
-        return p;
     }
+    return p;
 }
 fn main() {
     let repo = match Repository::open("./") {
@@ -17,13 +16,18 @@ fn main() {
         Err(e) => panic!("failed to open: {}", e),
     };
     let head = repo.head().unwrap();
-    let mut revwalk = repo.revwalk().unwrap();
-    let base = git2::Sort::REVERSE;
-    // let a = head.peel_to_commit().unwrap().parent(i)
-    for parent in head.peel_to_commit().unwrap().parents(){
-        println!("parent = {:?}", parent);
+    for commit in get_all_commits(head.peel_to_commit().unwrap()) {
+        println!(
+            "{} by {} @ {:?}",
+            commit.summary().unwrap(),
+            commit.author(),
+            commit.time().seconds()
+        );
     }
-    println!("commits: {:?}", get_prev_commit(head.peel_to_commit().unwrap()));
-    println!("Latest commit {}", head.peel_to_commit().unwrap().summary().unwrap());
+    // println!("commits: {:?}", get_all_commits(head.peel_to_commit().unwrap()));
+    println!(
+        "Latest commit {}",
+        head.peel_to_commit().unwrap().summary().unwrap()
+    );
     // println!("{}", repo.statuses(None).unwrap());
 }
