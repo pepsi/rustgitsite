890c71aa735acbc2f24cf2040933c4a0f6f7d8c1
Rusty Shackleford <a@neb.best>
2020-10-03 04:08:56 -0400

    now can print commit parents

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,18 @@ fn main() {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
     };
-    let b = repo.head().unwrap();
+    let head = repo.head().unwrap();
     let mut revwalk = repo.revwalk().unwrap();
     let base = git2::Sort::REVERSE;
+    // let a = head.peel_to_commit().unwrap().parent(i)
+    for parent in head.peel_to_commit().unwrap().parents(){
+        println!("parent = {:?}", parent);
+    }
     revwalk.set_sorting(base).unwrap();
     println!("st sorting to base");
     for walk in revwalk{
         println!("Walked {}", walk.unwrap());
     }
-    println!("Latest commit {}", b.peel_to_commit().unwrap().summary().unwrap());
+    println!("Latest commit {}", head.peel_to_commit().unwrap().summary().unwrap());
     // println!("{}", repo.statuses(None).unwrap());
 }
